f8750da8de1966fd90403ecdb6ee6c87c34cd864
imtaotao <dwchentao@163.com>
2019-09-20 10:14:30 +0800

    refactor: add new file

---
+++ build.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const path = require('path')
+const rollup = require('rollup')
+const rm = require('rimraf').sync
+const babel = require('rollup-plugin-babel')
+const cmd = require('rollup-plugin-commonjs')
+const cleanup = require('rollup-plugin-cleanup')
+const resolve = require('rollup-plugin-node-resolve')
+
+const entryPath = path.resolve(__dirname, './src/index.js')
+const outputPath = filename => path.resolve(__dirname, './dist', filename)
+
+const esm = {
+  input: entryPath,
+  output: {
+    file: outputPath('mpstore.esm.js'),
+    format: 'es',
+  }
+}
+
+const cjs = {
+  input: entryPath,
+  output: {
+    file: outputPath('mpstore.common.js'),
+    format: 'cjs',
+  }
+}
+
+async function build (cfg, sourcemap = false) {
+  cfg.output.sourcemap = sourcemap
+
+  const bundle = await rollup.rollup({
+    input: cfg.input,
+    plugins: [
+      cleanup(),
+      resolve(),
+      babel({
+        babelrc: true,
+        exclude: 'node_modules/**',
+      }),
+      cmd(),
+    ]
+  })
+  await bundle.generate(cfg.output)
+  await bundle.write(cfg.output)
+}
+
+console.clear()
+// delete old build files
+rm('./dist')
+
+const buildVersion = sourcemap => {
+  build(esm, sourcemap)
+  build(cjs, sourcemap)
+}
+
+// watch, use in dev and test
+if (process.argv.includes('-w')) {
+  let i = 0
+  fs.watch('./src', () => {
+    console.clear()
+    console.log('Rebuild: ' + ++i)
+    buildVersion(true)
+  })
+  buildVersion(true)
+} else {
+  buildVersion()
+}
